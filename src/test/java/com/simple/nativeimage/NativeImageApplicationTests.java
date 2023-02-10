222decc5d39e13491dd809d65fd13809c63beca0
Newnullboy1 <pumipas.n@gmail.com>
2023-02-11 00:50:45 +0700

    init project

---
+++ src/test/java/com/simple/nativeimage/NativeImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.nativeimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeImageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
